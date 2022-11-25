45aa02929d92111a15f92886c1fcd3ee67d17bc3
orchida17 <orchida.kuliyah2021@gmail.com>
2022-11-25 10:10:00 +0700

    membuat pustaka-booking

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,9 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h2>Perkenalkan</h2>";
+        echo"Nama Saya Bendi Saya tinggal di daerah Sragen olah raga yang saya sukai adalah Volly";
+    }
+}
